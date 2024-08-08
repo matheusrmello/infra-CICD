a72753eb2156d19a907f084f25beaf792c868e38
matheusrmello <matheus.rm.09@hotmail.com>
2024-08-08 20:32:43 -0300

    fix terraform

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-matheus"
+    bucket = "terraform-state-alura"
     key    = "Prod/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 }
